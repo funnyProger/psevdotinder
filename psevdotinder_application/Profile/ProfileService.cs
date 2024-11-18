0838888fe25aa6ef7cfabfee83ba8b47089e4f98
funnyProger <funbogdan2003@gmail.com>
2024-11-18 20:19:51 +0300

    add_little_changes

--- psevdotinder_application/Profile/ProfileService.cs
+++ psevdotinder_application/Profile/ProfileService.cs
@@ -6,6 +6,6 @@ public class ProfileService(IRepository<Profile> repository) : IService
     private IRepository<Profile> Repository { get; init; } = repository;
 
     public Task<IEnumerable<Profile>> GetProfileServiceAsync(User user, int page, int pageSize, CancellationToken cancellationToken = default) =>
-        Repository.GetPaged((profile =>profile.City == user.City && ((profile.Age - user.Age) >= -5 && (profile.Age - user.Age) <= 5)), page, pageSize, cancellationToken);
+        Repository.GetPaged((profile =>profile.City == user.City && ((profile.Age - user.Age) >= -5 && (profile.Age - user.Age) <= 5) && profile.Gender == user.Gender), page, pageSize, cancellationToken);
 }
 
--- psevdotinder_core/Entities/Profile.cs
+++ psevdotinder_core/Entities/Profile.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public int Age { get; set; }
         public string City { get; set; }
+        public string Gender { get; set; }
         public string Description { get; set; }
         public string AccountLink { get; set; }
     }
